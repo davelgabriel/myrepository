7c185a08e9da58ac5e301611959f492499671827
Higor Campos <higorkd@gmail.com>
2019-10-24 21:26:52 -0300

    Jogos para o hacktoberfest

---
+++ jogotabuada.py
@@ -0,0 +1,56 @@
+import os
+from random import randint
+
+def correta(z):
+	print()
+	print("OOPS!")
+	print("A resposta certa é", z)
+	print("Não se preocupe, só estudar mais!")
+	
+def limpa():
+	if os.name==("nt"):
+		os.system("cls")
+	else:
+		os.system("clear")
+
+def main(args):
+	print("Vamos ver se você é bom de tabuada: ")
+	i=1
+	print("NIVEL",i)
+	x = randint(0,5)
+	y = randint(0,5)
+	z = x*y
+	print("Qual a respota correta da multiplicação?")
+	resposta=int(input("{} x {} = ".format(x,y)))
+	cont=0
+	
+	if resposta == z:
+		limpa()
+		while resposta == z and cont<2:
+				i=1
+				print("NIVEL",i)
+				x = randint(0,5)
+				y = randint(0,5)
+				z = x*y
+				print("Qual a respota correta da multiplicação?")
+				resposta=int(input("{} x {} = ".format(x,y)))
+				limpa()
+				cont+=1
+	if cont == 2:
+		while resposta == z and cont<2:
+			i=2
+			print("NIVEL",i)
+			x = randint(0,5)
+			y = randint(0,5)
+			z = x*y
+			print("Qual a respota correta da multiplicação?")
+			resposta=int(input("{} x {} = ".format(x,y)))
+			limpa()
+			cont+=1
+	else:
+		correta(z)
+	return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
